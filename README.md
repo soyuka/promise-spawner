1791972a7d968f5ba6865d7aaa44c0a3e1301a30
soyuka <soyuka@gmail.com>
2014-05-24 15:23:39 +0200

    Init

--- README.md
+++ README.md
@@ -2,3 +2,75 @@ promise-spawner
 ===============
 
 Promise multiple spawner with stdout/stderr streams and error handling
+
+## Examples
+
+### Basic command example
+
+```javascript
+var spawner = new Spawner()
+
+spawner
+	.spawn('echo hello', 'echo world')
+	.then(function(code) {
+		expect(code).to.equal(0)
+		//to access out/err datas use
+		console.log(this.data.out, this.data.err)
+		//with this example, this.data.out will look like this:
+		expect(this.data.out).to.eql(['out: hello', 'out: world'])
+	})
+```
+
+### Basic failing command example
+
+```javascript
+var spawner = new Spawner()
+
+spawner
+	.spawn('exit 1', 'this will not be executed!')
+	.catch(function(code) {
+		console.log('Script failed with code ', code)
+	})
+```
+
+### Modifiers and streaming
+
+```javascript
+var modifiers = {
+	out: function(d) { return d },
+	err: 'this is an error: '
+}
+
+var spawner = new Spawner(modifiers)
+
+//spawner gives you global streams from spawned stdout and stderr
+spawner.out.pipe(stdout)
+spawner.err.pipe(stdout)
+
+spawner
+	//this will print "hello\n world\n err\n done\n" to the stdout
+	.spawn(['echo hello', 'echo world'], 'echo err >&2', ['sleep 0', 'echo done && exit 0'])
+	.then(function(code) {
+		exepect(this.data.err[0]).to.equal('err')
+
+		return spawner.spawn('echo next')
+	})
+	.then(function(code) {
+		expect(this.data.out[0]).to.equal('next')
+	})
+```
+
+## API
+
+#### `Spawner(options, spawn_options)`
+- options: modifiers: `{out: Function|String, err: Function|String}`
+- spawn_options: [http://nodejs.org/api/child_process.html](http://nodejs.org/api/child_process.html)
+
+#### `new Spawner()` 
+- spawn: `Function(command, ...)` where `command` can be an Array or a String - returns a `Promise`
+- sp: shortcut to spawn
+- out: global out stream
+- err: global err stream
+
+
+
---
+++ index.js
@@ -0,0 +1,139 @@
+var spawn = require('child_process').spawn
+  , through = require('through2')
+  , Promise = require('bluebird')
+  , platform = require('os').platform()
+
+/**
+ * Spawner - a wrapper to promised spawn
+ * @param  {Object}  options        modifiers {out: Function|string, err: Function|string}
+ * @param  {Object}  spawn_options  options to spawn process (nodejs.org/api/child_process.html)
+ * @return {Object}  Wrapper        {sp = spawn = Promise, Stream out, Stream err}
+ */
+var Spawner = function(options, spawn_options) {
+
+	var self = this
+	var opt = {out: 'out: ', err: 'err: '}
+	
+	if(typeof options == 'object') {
+		opt.out = options.out !== undefined ? options.out : opt.out
+		opt.err = options.err !== undefined ? options.err : opt.err
+	}
+
+	//those are global streams to allow piping of the current running spawn
+	self.out = this.pipe().on('data', function(d) { 
+		self.data.out.push(new Buffer(d).toString().replace('\n', '')) 
+	})
+	self.err = this.pipe().on('data', function(d) {
+		self.data.err.push(new Buffer(d).toString().replace('\n', ''))
+	})
+
+	var spawn = function() {
+
+		var args = [].slice.call(arguments)
+				args = [].concat.apply([], args) //flatten
+
+		var num_commands = args.length
+				, commands = []
+		    , i = 0
+
+		for(i; i < num_commands; i++) {
+			commands.push({
+			  command: self.command,
+				args: self.args.concat([args[i]]),
+				options: spawn_options || {}
+			})
+		}
+
+		//reset previous spawn datas
+		self.data = {out: [], err: []}
+
+		return new Promise(function(resolve, reject) {
+			var j = 0
+
+			//function to loop through promises commands
+			//doing like this to handle the catch promise when previous command fail and to reject spawner global promise
+			var loop = function(array) {
+				var command = array.shift()
+			
+				self
+					.promise_spawn.call(self, command, opt)
+					.then(function(code) {
+
+						if(j < num_commands - 1) {
+							j++
+							return loop(array)
+						} else {							
+							resolve(code)
+						}
+
+					}).catch(function(code) {
+						reject(code)
+					})
+			}
+
+			loop(commands)
+
+		}).bind(self)
+	}
+
+	//main function wrapper
+	return {
+		sp: spawn,
+		spawn: spawn,
+		out: self.out,
+		err: self.err
+	}
+}
+
+Spawner.prototype = {
+	//directly from nodejs exec code, allows to run string commands with ease
+	command: platform == 'win32' ? 'cmd.exe' : 'sh',
+	args: platform == 'win32' ? ['/s', '/c'] : ['-c'], 
+	/**
+	 * Promisify Spawn
+	 * @param  {Object} command {command, args, spawn_options}
+	 * @param  {Object} options modifiers from Spawner
+	 * @return {Promise}         Promise resolve on error code = 0 or reject
+	 */
+	promise_spawn: function(command, options) {
+		var self = this
+
+	  return new Promise(function(resolve, reject) {
+			
+			var s = spawn(command.command, command.args, command.options)
+
+			//spawn stdout to the Stream modifier
+			//writes data back to global Stream
+			s.stdout
+				.pipe(self.pipe(options.out))
+				.on('data', function(d) {
+					self.out.write(d)
+				})
+
+			s.stderr
+				.pipe(self.pipe(options.err))
+				.on('data', function(d) {
+					self.err.write(d)
+				})
+
+			s.on('close', function(code) {
+				if(code === 0)
+					resolve(code)
+				else
+					reject(code)
+			})
+	  })
+  
+	},
+	pipe: function(modifier) {
+		modifier = modifier === undefined ? '' : modifier
+
+		return through.obj(function (chunk, enc, callback) {
+			chunk = typeof modifier == 'function' ? modifier(chunk) : modifier + chunk
+	    this.push(chunk)
+	    callback()
+	  })
+	}
+}
+
+module.exports = Spawner
